422e2c0931337379a74920f4fd735be454957dfb
muhammadali010 <muhammadjonovmuhammadali32@gmail.com>
2024-09-28 16:02:55 +0500

    tugadi

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -20,7 +20,7 @@ const Products = () => {
     <div className="  p-8 grid grid-cols-1 md:grid-cols-3 gap-6 h-full">
       {products.map((product) => (
         <div key={product.id} className="p-1 rounded-lg shadow-2xl hover:shadow-xl transition-shadow" >
-          <img src={product.attributes.image} alt={product.attributes.name} className="rounded-lg mb-4 w-96 h-52" />
+          <img src={product.attributes.image} alt={product.attributes.name} className="rounded-lg mb-4 w-96 h-52 object-cover" />
           <h2 className="text-xl font-bold text-center">{product.attributes.title}</h2>
           <p className="text-violet-700 text-center">${product.attributes.price}</p>
         </div>
